563687ce81b2a1756dc5eaec8b1d9c812c83e68b
Manish Giri <itsmanishgiri@gmail.com>
2022-06-19 17:10:52 +0530

    Remove Header

--- app/src/main/java/com/example/yelpbusinessexample/data/repository/BusinessRepositoryImpl.java
+++ app/src/main/java/com/example/yelpbusinessexample/data/repository/BusinessRepositoryImpl.java
@@ -30,7 +30,7 @@ public class BusinessRepositoryImpl implements BusinessRepository {
 
     @Override
     public void getBusinesses(HashMap<String, String> queryMap) {
-        businessResponseCall = serviceApi.getBusinessesByDistance("Bearer XPFgzKwZGK1yqRxHi0d5xsARFOLpXIvccQj5jekqTnysweGyoIfVUHcH2tPfGq5Oc9kwKHPkcOjk2d1Xobn7aTjOFeop8x41IUfVvg2Y27KiINjYPADcE7Qza0RkX3Yx", queryMap);
+        businessResponseCall = serviceApi.getBusinessesByDistance("PUT YOUR HEADER HERE", queryMap);
         businessResponseCall.enqueue(new Callback<BusinessResponse>() {
             @Override
             public void onResponse(Call<BusinessResponse> call, Response<BusinessResponse> response) {
